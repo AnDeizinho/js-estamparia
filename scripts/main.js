be8bb4e36d4b146e473b83d94e8f26fb42870e1a
andrey <queirozandrey5@gmail.com>
2022-04-23 18:50:57 -0400

    corecao responsividade

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,1 @@
 
-function  Avancar()
-{
-        if(this.index < this.slides.length){
-            this.slides[this.index].style.marginLeft = "-" + ((100/6)*(this.index + 1)) + "%"
-            this.index++;
-        }
-}
-
